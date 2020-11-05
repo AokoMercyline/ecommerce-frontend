c9ba26e548f10e68f41fdd19f361761900d18442
AokoMercyline <aokomercyline34@gmail.com>
2020-11-05 11:52:03 +0300

    create Angular folder and generated navbar and login and logou and signup file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    SignupComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule
